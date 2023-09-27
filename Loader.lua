3294dbeb72e890dc8a763e44d87905e26f07feac
Nightkillerz <68940892+Nightkillerzz@users.noreply.github.com>
2023-09-26 22:39:59 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,20 +3,22 @@ if not game:IsLoaded() then
     game.Loaded:wait()
 end
 
--- Check the game's GameId
-if game.GameId == 4737765103 then
-    -- If the GameId matches, execute a remote script
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Nightkillerzz/-NK-/main/Murim%20Cultivation.lua", true))()
-elseif game.GameId == 0 then
-  -- Placeholder
-else
-    -- Handle the case when the GameId is not recognized
-    game:GetService("Players").LocalPlayer:Kick([[
-        .
-        Invalid game!
-        Join the discord for game list.
-        https://discord.gg/bTWphkwgZM
-        (It's been copied to clipboard too!)
-    ]])
-    setclipboard("https://discord.gg/bTWphkwgZM") -- Copy the discord link to clipboard
+if getgenv().NightKey == "FirstKeyForDiscordMembers" then
+    -- Check the game's GameId
+    if game.GameId == 4737765103 then
+        -- If the GameId matches, execute a remote script
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Nightkillerzz/-NK-/main/Murim%20Cultivation.lua", true))()
+    elseif game.GameId == 0 then
+        -- Placeholder
+    else
+        -- Handle the case when the GameId is not recognized
+        game:GetService("Players").LocalPlayer:Kick([[
+            .
+            Invalid game!
+            Join the discord for the game list.
+            https://discord.gg/bTWphkwgZM
+            (It's been copied to clipboard too!)
+        ]])
+        setclipboard("https://discord.gg/bTWphkwgZM") -- Copy the discord link to clipboard
+    end
 end
